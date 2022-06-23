3264e049dcf9070644833477358e9a84395bd9df
Shakirzhan <samarbaev99@mail.ru>
2022-06-23 16:14:49 +0300

    base drag board

--- ui/src/home/DragList.js
+++ ui/src/home/DragList.js
@@ -1,95 +1,106 @@
-import React, { useEffect } from "react";
+import * as React from 'react';
 import styled from "styled-components";
 import { DragDropContext } from "react-beautiful-dnd";
 import DraggableElement from "./DraggableElement";
 
 const DragDropContextContainer = styled.div`
-  padding: 20px;
-  border: 4px solid indianred;
-  border-radius: 6px;
+  height: 100%
 `;
 
 const ListGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 8px;
+  height: 100%;
+  overflow-y: scroll;
+  background-color: rgb(242 243 249 / 1); 
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  box-sizing: border-box;
 `;
 
-// fake data generator
-const getItems = (count, prefix) =>
-  Array.from({ length: count }, (v, k) => k).map((k) => {
-    const randomId = Math.floor(Math.random() * 1000);
-    return {
-      id: `item-${randomId}`,
-      prefix,
-      content: `item ${randomId}`
+const Wrap = styled.div`
+  padding: 1rem;
+  flex-direction: column;
+  height: 100vh;
+  display: flex;
+  box-sizing: border-box;
+`
+
+class DragList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: {
+        todo: [],
+        inProgress: [],
+        done: []
+      }
     };
-  });
+  }
 
-const removeFromList = (list, index) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(index, 1);
-  return [removed, result];
-};
+  removeFromList(list, index) {
+    const result = Array.from(list);
+    const [removed] = result.splice(index, 1);
+    return [removed, result];
+  };
+  
+  addToList(list, index, element) {
+    const result = Array.from(list);
+    result.splice(index, 0, element);
+    return result;
+  };
 
-const addToList = (list, index, element) => {
-  const result = Array.from(list);
-  result.splice(index, 0, element);
-  return result;
-};
-
-const lists = ["todo", "inProgress", "done"];
-
-const generateLists = () =>
-  lists.reduce(
-    (acc, listKey) => ({ ...acc, [listKey]: getItems(10, listKey) }),
-    {}
-  );
-
-function DragList() {
-  const [elements, setElements] = React.useState(generateLists());
-
-  useEffect(() => {
-    setElements(generateLists());
-  }, []);
-
-  const onDragEnd = (result) => {
+  onDragEnd(result) {
     if (!result.destination) {
       return;
     }
+    
+    const { elements } = this.state;
     const listCopy = { ...elements };
-
     const sourceList = listCopy[result.source.droppableId];
-    const [removedElement, newSourceList] = removeFromList(
+    const [removedElement, newSourceList] = this.removeFromList(
       sourceList,
       result.source.index
     );
     listCopy[result.source.droppableId] = newSourceList;
     const destinationList = listCopy[result.destination.droppableId];
-    listCopy[result.destination.droppableId] = addToList(
+    listCopy[result.destination.droppableId] = this.addToList(
       destinationList,
       result.destination.index,
       removedElement
     );
 
-    setElements(listCopy);
-  };
+    this.setState({ elements: listCopy })
+  }
 
-  return (
-    <DragDropContextContainer>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <ListGrid>
-          {lists.map((listKey) => (
-            <DraggableElement
-              elements={elements[listKey]}
-              key={listKey}
-              prefix={listKey}
-            />
-          ))}
-        </ListGrid>
-      </DragDropContext>
-    </DragDropContextContainer>
-  );
+  render() {
+    const { elements } = this.state;
+    const lists = ["todo", "inProgress", "done"];
+
+    return (
+      <Wrap>
+        <DragDropContextContainer>
+          <DragDropContext onDragEnd={(props) => this.onDragEnd(props)}>
+            <ListGrid>
+              {lists.map((listKey) => (
+                <DraggableElement
+                  elements={elements[listKey]}
+                  key={listKey}
+                  prefix={listKey}
+                />
+              ))}
+            </ListGrid>
+          </DragDropContext>
+        </DragDropContextContainer>
+      </Wrap>
+    );
+  }
 }
 
 export default DragList;
--- ui/src/home/DraggableElement.js
+++ ui/src/home/DraggableElement.js
@@ -11,22 +11,39 @@ const ColumnHeader = styled.div`
 const DroppableStyles = styled.div`
   padding: 10px;
   border-radius: 6px;
-  background: #d4d4d4;
+  background-color: rgb(228 229 236 / 1);
+  box-sizing: border-box;
+  max-height: 100%
+`;
+
+const names = {
+  "todo": "Сделать",
+  "inProgress": "В ходе выполнения",
+  "done": "Выполнено"
+}
+
+const Wrap = styled.div`
+  height: 100%;
+  & > div {
+    height: 100%;
+  }
 `;
 
 const DraggableElement = ({ prefix, elements }) => (
   <DroppableStyles>
-    <ColumnHeader>{prefix}</ColumnHeader>
-    <Droppable droppableId={`${prefix}`}>
-      {(provided) => (
-        <div {...provided.droppableProps} ref={provided.innerRef}>
-          {elements.map((item, index) => (
-            <ListItem key={item.id} item={item} index={index} />
-          ))}
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+    <ColumnHeader>{names[prefix]}</ColumnHeader>
+    <Wrap>
+      <Droppable droppableId={`${prefix}`}>
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {elements.map((item, index) => (
+              <ListItem key={item.id} item={item} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </Wrap>
   </DroppableStyles>
 );
 
